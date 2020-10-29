5f9239e5758c2020416c39b5d926a11da0181ba1
reliztrent <rtrent@loc.gov>
2020-10-29 12:27:10 -0400

    add popup functionality to cards, #37

--- docs/assets/navbar.js
+++ docs/assets/navbar.js
@@ -1,3 +1,7 @@
+/*
+Card glow
+*/
+
 $('.sidebar .nav-link').click(function(){
     $('.glow').removeClass('glow');
     var anchor_href = $(this).attr('href');
@@ -8,3 +12,54 @@ $('.sidebar .nav-link').click(function(){
         anchor_id.removeClass('glow');
     }, 3000); /This should match the number of seconds on the glow css class/
 })
+
+/* 
+Card zoom
+*/
+
+$('.zoom').click(function(){
+	/* get parent card */
+	var card = $(this).closest(".card");
+	/* calculate for a new height-based width */
+	var cardh = $(card).height();
+	var cardw = $(card).width();
+	var cardp = cardw/cardh
+	var wh = $(window).height();
+	var ww = $(window).width();
+	var neww = wh * cardp;
+	/* apply the zoom */
+	$(card)
+	  .width(neww)
+	  .css('position','fixed')
+	  .css('left',(ww-neww)/2)
+	  .css('z-index','2000')
+	  .css('top','2rem');
+    /* add a close button */
+	$(card).prepend(
+		  '<div class="modal-header">'+
+	        '<button type="button" class="close card-close p-0" aria-label="Close">'+
+	          '<span aria-hidden="true">×</span>'+
+	        '</button>'+
+	      '</div>'
+	      );
+	/* remove the top-right buttons */
+	$(this).closest('div.top-right').hide();
+
+	$('.card-close').children('span').click(function(){
+	/* get parent card */
+		var card = $(this).closest(".card");
+		
+		/* remove zoom properites */
+		$(card)
+			.css('width','')
+			.css('position','')
+			.css('left','')
+			.css('z-index','')
+			.css('top','');
+	    /* remove close btn and restore zoom btn */
+		$(card).children('.modal-header').remove();
+		/* restore close btn and restore zoom btn */
+		$(card).find('div.top-right').show();
+	})
+});
+
